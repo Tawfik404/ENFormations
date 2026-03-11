5cda57edab10cd3ae1bddfce15b6b90eaa9d1d00
Tawfik <tawfikabouhmad.404@gmail.com>
2026-03-11 21:56:52 +0000

    added models, migrations and defined the admin table

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,23 @@
 <?php
 
+use App\Http\Controllers\DivisionController;
+use App\Http\Controllers\FormateurController;
+use App\Http\Controllers\FormationController;
+use App\Http\Controllers\ModuleController;
+use App\Http\Controllers\SalleController;
+use App\Http\Controllers\StagiaireController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+//API routes
+Route::apiResources([
+    'stagiaire' => StagiaireController::class,
+    'division' => DivisionController::class,
+    'formateur' => FormateurController::class,
+    'formation' => FormationController::class,
+    'salle' => SalleController::class,
+    'module' => ModuleController::class,
+]);
+
 
